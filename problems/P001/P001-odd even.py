cf2d98433ff391921d613ca8d6c2d95c7e837f56
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-07-06 19:27:40 +0330

    Added P001 test

--- problems/P001/P001-odd even.py
+++ problems/P001/P001-odd even.py
@@ -1,5 +1,10 @@
-a = int(input())
-if a % 2 == 0:
-    print(f'{a} is even')
-else : 
-    print(f'{a} is odd')
+def even_odd(number):
+    if a % 2 == 0:
+        return(f'{number} is even')
+    else : 
+        return(f'{number} is odd')
+
+if __name__ == "__main__":
+    a = int(input())
+    answer = even_odd(a)
+    print(answer)
