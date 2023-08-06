740a704cd828ca88972a7003ee9a57e9127edfb8
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-06 20:50:20 +0330

    Added string on input P016

--- problems/P016/P016.py
+++ problems/P016/P016.py
@@ -5,7 +5,7 @@ def slohsen(number):
         m.append(d * b)
     return m
 if __name__ == '__main__':
-    a = int(input())
+    a = int(input("Enter the number: "))
     answer = slohsen(a)
     for b in answer:
         print(b)
