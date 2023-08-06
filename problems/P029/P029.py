38aa9d6544614803a3c8335173bae92023486155
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-06 15:12:02 +0330

    Added P029

--- problems/P029/P029.py
+++ problems/P029/P029.py
@@ -6,36 +6,39 @@ for A in range(0 , a):
 H = 0
 for total in m:
     H = H + total
+
 I = 1
 for beat in m:
     I = I * beat
 
-Maxis = []
+Max_is = []
 for MAX in range(1 , H):
     for max in range(1 , a):
         Max = m[max - 1]
         if Max == MAX:
-            Maxis.append(Max)
-Minis = []
+            Max_is.append(Max)
+
+Min_is = []
 for MIN in range(H , 0 , -1):
     for min in range(a , 0 , -1):
         Min = m[min - 1]
         if Min == MIN:
-            Minis.append(Min)
-Avgis = []
+            Min_is.append(Min)
+
+Avg_is = []
 h = 1
 for AVG in m:
     h = h +AVG
-    Avgis.append(h)
-h = int(h / len(Avgis))
+    Avg_is.append(h)
+h = int(h / len(Avg_is))
 
-lastthreenumbersareis = []
-for lastthreenumbersare in range(len(m) - 3 , len(m)):
-    lastthreenumbersareis.append(m[lastthreenumbersare])
+Last_three_numbers_are_is = []
+for last_three_numbers_are in range(len(m) - 3 , len(m)):
+    Last_three_numbers_are_is.append(m[last_three_numbers_are])
     
-print(f"Max is {int(Maxis[len(Maxis) - 1])}")
-print(f"Min is {int(Minis[len(Minis) - 1])}")
+print(f"Max is {int(Max_is[len(Max_is) - 1])}")
+print(f"Min is {int(Min_is[len(Min_is) - 1])}")
 print(f"Avg is {h}")
-print(f"Last 3 numbers are is {lastthreenumbersareis}")
+print(f"Last 3 numbers are is {Last_three_numbers_are_is}")
 print(f"Sum of all is {H}")
 print(f"Product of all is {I}")
