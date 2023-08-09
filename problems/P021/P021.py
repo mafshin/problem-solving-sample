7607a1c81185b0bbe8385fb7451d829e47ac495a
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-09 14:18:01 +0330

    Added P021 and P018

---
+++ problems/P021/P021.py
@@ -0,0 +1,9 @@
+a = {'mercury' : 57 , 'venus' : 109 , 'earth' : 150 , 'mars' : 288 , 'jupiter' : 779 , 'saturn' : 1430 , 'uranus' : 2880 , 'neptune' : 4500}
+print(f"Sun{int(a['mercury'] / a['mercury']) * '-'}Mercury")
+print(f"Sun{int(a['venus'] / a['mercury']) * '-'}Venus")
+print(f"Sun{int(a['earth'] / a['mercury']) * '-'}Earth")
+print(f"Sun{int(a['mars'] / a['mercury']) * '-'}Mars")
+print(f"Sun{int(a['jupiter'] / a['mercury']) * '-'}Jupiter")
+print(f"Sun{int(a['saturn'] / a['mercury']) * '-'}Saturn")
+print(f"Sun{int(a['uranus'] / a['mercury']) * '-'}Uranus")
+print(f"Sun{int(a['neptune'] / a['mercury']) * '-'}Neptune")
