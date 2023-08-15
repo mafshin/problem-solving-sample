470a5adcaf1e62d8c2195622e66f6ddd1eca991e
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-15 16:31:27 +0330

    Added P018_test

--- problems/P018/P018.py
+++ problems/P018/P018.py
@@ -1,11 +1,18 @@
-a = int(input())
-def ispreme (a):
-    ispreme = True
-    for d in range(2 , a):
-        if a % d == 0:
-            ispreme = False
-            break
-    return ispreme
-for c in range(2 , a):
-    if ispreme(c) and ispreme(c + 2):
-        print(c , c + 2)
+def P018(number):
+    m = []
+    def is_preme(number):
+        ispreme = True
+        for d in range(2 , number):
+            if number % d == 0:
+                ispreme = False
+                break
+            return ispreme
+    for c in range(2 , number):
+        if is_preme(c) and is_preme(c + 2):
+            m.append((c , c + 2))
+    return m
+if __name__ == "__main__":
+    a = int(input())
+    answer = P018(a)
+    for c in answer:
+        print(c)
