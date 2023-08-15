e26feaf55e1e070697de923e7e493f6ed3f11760
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-15 17:33:05 +0330

    Added P031_test

--- problems/P031/P031.py
+++ problems/P031/P031.py
@@ -1,19 +1,26 @@
-a = int(input("input : "))
-if a < 2:
-    print(f"{a} < 2")
-else:
-    m = []
-    for b in range(2 , a):
-        is_preme = True
-        for d in range(2 , b):
-            if b % d ==0:
-                is_preme = False
-        if is_preme:
-            m.append(b)
-            for X in range(len(m)):
-                for Y in range(1 , len(m)):
-                    if m[X] + m[Y] == a:
-                        if a % 2 ==0:
-                            print(m[X] , m[Y])
-                        if a % 2 !=0:
-                            print(f"{a} is odd")
+def P031(number):
+    n = []
+    if number < 2:
+        return f"{number} < 2"
+    else:
+        m = []
+        for b in range(2 , number):
+            is_preme = True
+            for d in range(2 , b):
+                if b % d ==0:
+                    is_preme = False
+            if is_preme:
+                m.append(b)
+                for X in range(len(m)):
+                    for Y in range(1 , len(m)):
+                        if m[X] + m[Y] == number:
+                            if number % 2 ==0:
+                                n.append((m[X] , m[Y]))
+                            if number % 2 !=0:
+                                n.append(f"{number} is odd")
+    return n
+if __name__ == "__main__":
+    a = int(input("int : "))
+    answer = P031(a)
+    for b in answer:
+        print(b)
