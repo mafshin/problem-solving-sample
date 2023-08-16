42317dc095305fd93b337d6273710bf0888150e5
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-08-16 11:45:56 +0330

    Added P047_test

--- problems/P047/P047_test.py
+++ problems/P047/P047_test.py
@@ -4,10 +4,10 @@ def test_1():
     assert P047(10) == 4
 
 def test_2():
-    assert P047(1000000) == 78498
+    assert P047(100) == 25
 
 def test_3():
-    assert P047(1000000000) == 50847534
+    assert P047(1000) == 168
 
 def test_4():
-    assert P047(100000000000) == 4118054813
+    assert P047(10000) == 1229
