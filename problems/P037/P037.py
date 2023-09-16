cf4ad7f9ed362536853946914d4efb0b3136a0bb
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-09-16 19:05:31 +0330

    Added P037

--- problems/P037/P037.py
+++ problems/P037/P037.py
@@ -1,30 +1,34 @@
-a = input("input : ").split(' ')
-m = []
-A = 1
-while True:
-    b = input(f"input {A} : ")
-    if b == 'END':
-        break
-    m.append(b)
-    A += 1
-for x1 in a:
-    for x2 in m:
-        if x1 == x2:
-            a.remove(x1)
-        if x1.endswith('.'):
-            if x1 == x2 + '.':
-                a.insert(a.index(x1) , '.')
-                a.remove(x1)
-        if x1.endswith(','):
-            if x1 == x2 + ',':
-                a.insert(a.index(x1) , ',')
-                a.remove(x1)
-        if x1.endswith('!'):
-            if x1 == x2 + '!':
-                a.insert(a.index(x1) , '!')
-                a.remove(x1)
-        if x1.endswith('?'):
-            if x1 == x2 + '?':
-                a.insert(a.index(x1) , '?')
-                a.remove(x1)
-print(str(" ".join(a)))
+def P037(number):
+    m = []
+    A = 1
+    while True:
+        b = input(f"input {A} : ")
+        if b == 'END':
+            break
+        m.append(b)
+        A += 1
+    for x1 in number:
+        for x2 in m:
+            if x1 == x2:
+                number.remove(x1)
+            if x1.endswith('.'):
+                if x1 == x2 + '.':
+                    number.insert(number.index(x1) , '.')
+                    number.remove(x1)
+            if x1.endswith(','):
+                if x1 == x2 + ',':
+                    number.insert(number.index(x1) , ',')
+                    number.remove(x1)
+            if x1.endswith('!'):
+                if x1 == x2 + '!':
+                    number.insert(number.index(x1) , '!')
+                    number.remove(x1)
+            if x1.endswith('?'):
+                if x1 == x2 + '?':
+                    number.insert(number.index(x1) , '?')
+                    number.remove(x1)
+    return str(" ".join(number))
+if __name__ == "__main__":
+   a = input("input : ").split(' ')
+   answer = P037(a)
+   print(answer)
