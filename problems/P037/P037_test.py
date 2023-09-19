6a091eb26d133386477b41f308d15c8b2793e99d
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-09-19 21:00:25 +0330

    Added P037_test

--- problems/P037/P037_test.py
+++ problems/P037/P037_test.py
@@ -1,7 +1,5 @@
 from P037 import *
 
-
-
 def test1():
     a = 'Today is president resignation date, a vantage point in history of our country. You, the next generation of this soil, will decide who should take control of your country.'
 
