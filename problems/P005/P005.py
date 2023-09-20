2c3dcf73536ee82a265c052839447fe443d613eb
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-09-20 17:51:53 +0330

    Added P005_test

--- problems/P005/P005.py
+++ problems/P005/P005.py
@@ -1,11 +1,16 @@
-a = int(input())
-b = input()
-c = int(input())
-if b == '+':
-   print(a + c)
-if b == '-':
-    print(a - c)
-if b == '*':
-    print(a * c)
-if b == '/':
-    print(a / c)
+def P005(number_1 , number_2 , number_3):
+    if number_2 == '+':
+        return number_1 + number_3
+    if number_2 == '-':
+        return number_1 - number_3
+    if number_2 == '*':
+        return number_1 * number_3
+    if number_2 == '/':
+        return number_1 / number_3
+if __name__ == "__main__":
+    a = int(input())
+    b = input()
+    c = int(input())
+    answer = P005(a , b , c)
+    print()
+    print(answer)
---
+++ problems/P005/P005_test.py
@@ -0,0 +1,7 @@
+from P005 import *
+
+def test1():
+    assert P005(2 , '+' , 3) == 5
+
+def test2():
+    assert P005(6 , '/' , 3) == 2
