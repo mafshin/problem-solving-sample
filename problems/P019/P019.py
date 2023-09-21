fa43a99ad2219f37df4784f74f331ad6dd5ee8cd
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-09-21 17:08:46 +0330

    Added P019_test

--- problems/P019/P019.py
+++ problems/P019/P019.py
@@ -1,34 +1,37 @@
-INPUT = int(input('int : '))
-H = INPUT * 2
-a = [1]
-b = [1 , 1]
-if len(a) <= INPUT:
-    print('.' * H , a , '.' * H)
-    H -= 1
-if len(b) <= INPUT:
-    print('.' * H , b , '.' * H)
-while len(a) <= INPUT and len(b) <= INPUT:
-    H -= 1
+def P019(number_1):
+    ouput = []
     a = [1]
-    if len(b) <= INPUT:
-        x = 0
-        y = 1
-        while y < len(b):
-            a.append(b[x] + b[y])
-            x += 1 
-            y += 1
-        a.append(1)
-        if len(a) <= INPUT :
-            print('.' * H , a , '.' * H)
-            H -= 2
-    if len(a) <= INPUT:
-        b = [1]
-        x = 0
-        y = 1
-        while y < len(a):
-            b.append(a[x] + a[y])
-            x += 1
-            y += 1
-        b.append(1)
-        if len(b) <= INPUT:
-            print('.' * H , b , '.' * H)
+    b = [1 , 1]
+    if len(a) <= number_1:
+        ouput.append(a)
+    if len(b) <= number_1:
+        ouput.append(b)
+    while len(a) <= number_1 and len(b) <= number_1:
+        a = [1]
+        if len(b) <= number_1:
+            x = 0
+            y = 1
+            while y < len(b):
+                a.append(b[x] + b[y])
+                x += 1 
+                y += 1
+            a.append(1)
+            if len(a) <= number_1 :
+                ouput.append(a)
+        if len(a) <= number_1:
+            b = [1]
+            x = 0
+            y = 1
+            while y < len(a):
+                b.append(a[x] + a[y])
+                x += 1
+                y += 1
+            b.append(1)
+            if len(b) <= number_1:
+                ouput.append(b)
+    return ouput
+if __name__ == "__main__":
+    INPUT = int(input('int : '))
+    answer = P019(INPUT)
+    for IXI in answer:
+        print(IXI)
