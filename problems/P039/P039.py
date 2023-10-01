a2a9d30b7ef17352a2f1360675f75ff2d99af209
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-10-01 19:03:12 +0330

    Added P039_test

--- problems/P039/P039.py
+++ problems/P039/P039.py
@@ -1,21 +1,27 @@
-a = int(input('int : '))
-n = []
-for A in range(1 , a + 1):
-    b = input(f'list {A} : ')
-    n.append(b.split())
-m = []
-for w in range(len(n)):
-    for x in n[w]:
-        I = 0
-        for W in range(1 , len(n)):
-            for X in n[W]:
-                if x == X:
-                    I += 1
-                    if I == a - 1:
-                        if x not in list(map(lambda f: f[0] , m)):
-                            m.append((x , I))
-m.sort(key=lambda p: -1 * p[1])
-ouput = []
-for OUPUT in m:
-    ouput.append(OUPUT[0])
-print(" ".join(ouput))
+def P039(number_1, list_1):
+    m = []
+    for w in range(len(list_1)):
+        for x in list_1[w]:
+            I = 0
+            for W in range(len(list_1)):
+                for X in set(list_1[W]):
+                    if x == X:
+                        I += 1
+                        if I == number_1:
+                            u = list(map(lambda f: f[0] , m))
+                            if x not in u:
+                                m.append((x , I))
+    m.sort(key=lambda p: -1 * p[1])
+    ouput = []
+    for OUPUT in m:
+        ouput.append(OUPUT[0])
+    return " ".join(ouput)
+
+if __name__ == '__main__':
+    a = int(input('int : '))
+    n = []
+    for A in range(1 , a + 1):
+        b = input(f'list {A} : ')
+        n.append(b.split())
+    answer = P039(a , n)
+    print(answer)    
