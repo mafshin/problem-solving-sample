12849846febda688352f73e00058f9ea12e4f55f
AbbasSaberi <abbasbazicomputer@gmail.com>
2023-11-07 18:45:38 +0330

    Added P002_ui

--- problems/P002/P002_test.py
+++ problems/P002/P002_test.py
@@ -1,7 +1,7 @@
 from P002 import *
 
 def test1():
-    assert P002(10 , 5) == (15, 5, 50, 2)
+    assert function(10 , 5) == (15, 5, 50, 2)
 
 def test2():
-    assert P002(6 , 4) == (10, 2, 24, 1)
+    assert function(6 , 4) == (10, 2, 24, 1)
